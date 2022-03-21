a32770552c52794aed3e2234beafff15fb5daefb
Louise Thomsen <loui718d@stud.kea.dk>
2022-03-21 17:55:37 +0100

    hjul og forside

--- style.js
+++ style.js
@@ -33,14 +33,3 @@ function toggleMenu() {
 
 /* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
 btn.addEventListener("click", toggleMenu);
-
-// lykkehjul
-
-const hjul = document.querySelector("#hjul");
-
-hjul.addEventListener("click", hjulDrej);
-
-function hjulDrej() {
-  console.log("hej");
-  document.querySelector("#hjul").classList.add(".drej");
-}
